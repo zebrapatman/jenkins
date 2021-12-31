3b91e16b7813d5c652044d84a10616d88a78395c
Patrick Van Herck <patrick_vanherck@icloud.com>
2021-12-31 14:31:27 +0100

    vijfde commit met jacoco

--- src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
@@ -17,4 +17,13 @@ public class GoedeDoel {
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
+
+	@Override
+	public boolean equals(Object object){
+		if (object instanceof GoedeDoel){
+			var ander = (GoedeDoel) object;
+			return naam.equalsIgnoreCase(ander.naam);
+		}
+		return false;
+	}
 }
