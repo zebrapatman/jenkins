b8b7305fcf381dcebde7ee85e32fca7d0bdd6cff
Patrick Van Herck <patrick_vanherck@icloud.com>
2022-01-03 12:47:54 +0100

    commit 10 javadoc

--- src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
@@ -2,18 +2,34 @@ package be.vdab.goededoel.domain;
 
 import java.math.BigDecimal;
 
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt
+ * @author Joe Dalton
+ */
 public class GoedeDoel {
 	private final String naam;
 	private BigDecimal opgebracht = BigDecimal.ZERO;
 
+	/**
+	 * Maakt een GoedeDoel object
+	 * @param naam De naam van het goede doel
+	 */
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
 
+	/**
+	 * Geeft de naam terug
+	 * @return  de naam
+	 */
 	public String getNaam() {
 		return naam;
 	}
 
+	/**
+	 * Geeft de opbrengst terug
+	 * @return de opbrengst
+	 */
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
